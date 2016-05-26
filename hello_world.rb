4e74df26f6e47099e2dd791456817d52d8667cd3
suliman <solomongamid@users.noreply.github.com>
2016-05-26 14:22:35 +0200

    suli

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,15 @@
 class HelloWorld
    def self.hello
-       #
-       # YOUR CODE GOES HERE
-       #
+           class HelloWorld
+   def initialize(name)
+      @name = name.capitalize
+   end
+   def sayHi
+      puts "Hello #{@name}!"
+   end
+end
+
+hello = HelloWorld.new("World")
+hello.sayHi
    end
 end 
